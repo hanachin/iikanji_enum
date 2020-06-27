8f5f6d269d7f0a595e3430359a25655209fbfccf
Seiei Miyagi <hanachin@gmail.com>
2020-06-28 01:33:53 +0900

    Update Post::Type.seed

--- app/models/post/type.rb
+++ app/models/post/type.rb
@@ -1,8 +1,10 @@
 class Post < ApplicationRecord
   class Type < ApplicationRecord
-    def self.seed
-      [PublishedPost, DraftPost].each do |klass|
-        find_or_create_by!(id: klass.name)
+    class << self
+      def seed
+        [PublishedPost, DraftPost].each do |klass|
+          find_or_create_by!(id: klass.name)
+        end
       end
     end
   end
