2eed8027fd7a63ce21bb9e5564a65194f4c2652f
Seiei Miyagi <hanachin@gmail.com>
2020-06-28 01:09:56 +0900

    Change post_types id column type to string, and add foreign key from posts.type to post_types

--- db/migrate/20200627160353_create_post_types.rb
+++ db/migrate/20200627160353_create_post_types.rb
@@ -1,8 +1,10 @@
 class CreatePostTypes < ActiveRecord::Migration[6.0]
   def change
-    create_table :post_types do |t|
+    create_table :post_types, id: :string do |t|
 
       t.timestamps
     end
+
+    add_foreign_key :posts, :post_types, column: :type
   end
 end
