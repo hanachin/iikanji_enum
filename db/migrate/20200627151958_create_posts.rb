b88e0d366f632f5f7965d1cac2dc5938df67c619
Seiei Miyagi <hanachin@gmail.com>
2020-06-28 01:45:50 +0900

    Simplify migration

--- db/migrate/20200627151958_create_posts.rb
+++ db/migrate/20200627151958_create_posts.rb
@@ -8,5 +8,17 @@ class CreatePosts < ActiveRecord::Migration[6.0]
 
       t.timestamps
     end
+
+    create_table :post_states do |t|
+      t.string :name
+
+      t.timestamps
+    end
+    add_foreign_key :posts, :post_states, column: :state
+
+    create_table :post_types, id: :string do |t|
+      t.timestamps
+    end
+    add_foreign_key :posts, :post_types, column: :type
   end
 end
