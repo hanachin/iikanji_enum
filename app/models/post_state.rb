f45f632ceef6771288d49bea29ff29e9df995316
Seiei Miyagi <hanachin@gmail.com>
2020-06-28 00:53:30 +0900

    Add PostState.seed to create seed records of PostState

--- app/models/post_state.rb
+++ app/models/post_state.rb
@@ -1,2 +1,9 @@
 class PostState < ApplicationRecord
+  class << self
+    def seed
+      Post.states.each do |state, id|
+        find_or_create_by!(id: id, name: state)
+      end
+    end
+  end
 end
